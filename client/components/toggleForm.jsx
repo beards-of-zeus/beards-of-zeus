b85c9fda3478435a57408621ac0e2521f960a676
Jonathan Mason <jmason.p@gmail.com>
2015-08-10 16:46:34 -0500

    # This is a combination of 10 commits. # The first commit's message is: Adds placeholder button.
    # This is the 2nd commit message:
    
    Adds proper commenting.
    
    # This is the 3rd commit message:
    
    Adds initial component.
    
    # This is the 4th commit message:
    
    Adds createActivity form to main.
    
    # This is the 5th commit message:
    
    Adds intial component.
    
    # This is the 6th commit message:
    
    Adds toggleForm component, removes placeholder button.
    
    # This is the 7th commit message:
    
    Adds toggle function to form.
    
    # This is the 8th commit message:
    
    Adds toggle feature to form.
    
    # This is the 9th commit message:
    
    Removes placeholder form.
    
    # This is the 10th commit message:
    
    Adds changing button text on toggle.

---
+++ client/components/toggleForm.jsx
@@ -0,0 +1,23 @@
+var Create = require('./createActivity.jsx');
+module.exports = React.createClass({
+  displayName: 'ToggleForm',
+  getInitialState: function(){
+    return {
+      showForm: false
+    };
+  },
+  onClick: function(){
+    this.setState({
+      showForm: !this.state.showForm
+    });
+    console.log(this.state.showForm);
+  },
+  render: function(){
+    return (
+      <div>
+        <button onClick={this.onClick}>{this.state.showForm ? 'Cancel' : 'Add an Activity'}</button>
+        {this.state.showForm ? <Create /> : null}
+      </div>
+      );
+  }
+});
