a797a69a0ec36e8ebf9469ecaea0c7449fe00c19
Owen Dismuke <owen.dismuke@gmail.com>
2015-08-11 16:56:05 -0700

    Connection string refactor for mysql db. Added 16x16 logo

--- server/db/database.js
+++ server/db/database.js
@@ -1,8 +1,5 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('olympus', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+var sequelize = new Sequelize(process.env.MYSQL || 'mysql://root@localhost:3306/olympus');
 
 module.exports = sequelize;
