a067a675f7bfbcaf0637ae91d3455f77a2756abe
Jonathan Mason <jmason.p@gmail.com>
2015-08-11 17:24:01 -0500

    Adds correct post url.

--- client/components/createActivity.jsx
+++ client/components/createActivity.jsx
@@ -4,7 +4,7 @@ module.exports = React.createClass({
     return (
         <div>
           <h3>Create Your Activity</h3>
-          <form action="/postActivity" method="post">
+          <form action="/data/activities" method="post">
             <label>Title: </label><input type="text" name="title" ref="title"/>
             <label>Description: </label><textarea name="description" ref="description"></textarea>
             <label>Location: </label><input type="text" name="location" ref="location"/>
