a8ce3640043791278afc277cd2dee7ec876cc21a
Marq Short <marqshort@gmail.com>
2015-08-15 01:30:11 -0400

    Added active filter to query.

--- server/server-config.js
+++ server/server-config.js
@@ -55,10 +55,10 @@ app.get('/data/activities', function(req, res){
 
   //query for all activities for which the user is NOT an owner
   sequelize.query("select id, description, title, keywords, location, " + 
-    "(select picture from Users user where userId = activity.ownerIdUserId) " + 
-     "as avatar, (select name from Users user where userId = " +
-      "activity.ownerIdUserId) as owner from Activities activity " + 
-    "where ownerIdUserId != '" + req.query.userID + "'", 
+    "(select picture from Olympus.Users user where userId = activity.ownerIdUserId) " + 
+     "as avatar, (select name from Olympus.Users user where userId = " +
+      "activity.ownerIdUserId) as owner from Olympus.Activities activity " + 
+    "where ownerIdUserId != '" + req.query.userID + "' and active = true", 
     { type: sequelize.QueryTypes.SELECT}).then(function(results){
       res.send(results);
   });
